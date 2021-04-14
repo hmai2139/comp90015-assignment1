5ea1e0f5c2cbd55d02558033347f6caf67af98d1
Hoang Mai <hoangmai2139@gmail.com>
2021-04-15 03:17:45 +1000

    Added basic Server and Client program.

--- src/assignment1/Client.java
+++ src/assignment1/Client.java
@@ -1,5 +1,33 @@
+/*
+** Dictionary client.
+ */
 package assignment1;
 
+// Dependencies.
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Client {
-    
+    public static void main(String args[]) throws IOException {
+        // Get server and port from STDIN.
+        final String SERVER = args[0];
+        final int PORT = Integer.parseInt(args[1]);
+
+        // Open connection to the dictionary server, at port PORT.
+        Socket socket = new Socket(SERVER, PORT);
+        System.out.println("Connected to " + SERVER + " at port " + PORT);
+
+        // Obtaining input and output streams from the socket.
+        // Create I/O streams for communicating to the client.
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        System.out.println(in.readUTF());
+
+        // All done, close connection and exit.
+        in.close();
+        out.close();
+        socket.close();
+    }
 }
--- src/assignment1/Server.java
+++ src/assignment1/Server.java
@@ -1,9 +1,40 @@
+/*
+** Multi-thread dictionary server.
+ */
 package assignment1;
+
+// Dependencies.
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.net.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class Server {
 
-    public static void main(String[] args) {
-	// test test
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+
+        // Get port from STDIN.
+        final int PORT = Integer.parseInt(args[0]);
+
+        // Open the Server Socket.
+        ServerSocket server = new ServerSocket(PORT);
+        System.out.println("Server started, waiting for request...");
+        // Wait for the Client Request.
+        Socket client = server.accept();
+        System.out.println("A new client is connected : " + client);
+
+        // Create I/O streams for communicating to the client.
+        DataInputStream in = new DataInputStream(client.getInputStream());
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+        // Send message to client upon connection.
+        out.writeUTF("Greetings from server :D");
+
+        // Close the connection, but not the server socket.
+        out.close();
+        in.close();
+        client.close();
     }
 }
